372bde1e0e9a99c3ecb8372bfe38721b9942b494
SheylaPDV <sheylapdv@gmail.com>
2021-11-07 13:32:35 +0100

    tabla movimientos

---
+++ balance/modelo.py
@@ -0,0 +1,10 @@
+class Movimiento():
+    def __init__(self, fecha, hora, concepto, ingreso_o_gasto, cantidad):
+        self.fecha = fecha
+        self.hora = hora
+        self.concepto = concepto
+        self.ingreso_o_gasto = ingreso_o_gasto
+        self.cantidad = cantidad
+        pass
+
+class ListadoMovimientos:
