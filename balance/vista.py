5cf85d02559426271f94bb6be25e037a426b5a21
SheylaPDV <sheylapdv@gmail.com>
2021-11-07 17:47:11 +0100

    añado html

--- balance/vista.py
+++ balance/vista.py
@@ -1,5 +1,5 @@
 # aqui poner las rutas y lo que hay que hacer en ellas
-
+from flask import render_template
 from . import app
 from .modelo import ListadoMovimientos
 
@@ -7,4 +7,7 @@ from .modelo import ListadoMovimientos
 def mostrar_tabla():
     lista_movimientos = ListadoMovimientos()
     lista_movimientos.leer_lista()
-    return lista_movimientos.movimientos
+    #return lista_movimientos.movimientos
+    # muestra la plantilla en pantalla, en este caso de inicio-html
+
+    return render_template("inicio.html", items=lista_movimientos.movimientos)
