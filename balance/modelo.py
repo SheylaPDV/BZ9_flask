048ea938989103d83a310cf999f5f9a36844669a
SheylaPDV <sheylapdv@gmail.com>
2021-11-08 13:09:41 +0100

    realizo cambios css y html

--- balance/modelo.py
+++ balance/modelo.py
@@ -1,13 +1,23 @@
+from datetime import date
+from datetime import datetime
 from . import FICHERO
 import csv
 
 
 class Movimiento():
-    def __init__(self, fecha, concepto, ingreso_gasto, cantidad):
-        self.fecha = fecha
-        self.concepto = concepto
-        self.ingreso_gasto = ingreso_gasto
-        self.cantidad = cantidad
+    def __init__(self, linea):
+        self.errores = []
+        ahora = datetime.now()
+        try:
+            self.fecha = date.fromisoformat(linea["fecha"])
+            if self.fecha.strftime("%Y%m%d") > ahora.strftime("%Y%m%d"):
+                self.errores.append("La fecha no puede ser futura")
+        except ValueError:
+            self.errores.apend("Formato de fecha erroneo")
+        self.hora = linea["hora"]
+        self.concepto = linea["concepto"]
+        self.ingreso_gasto = linea["ingreso_gasto"]
+        self.cantidad = linea["cantidad"]
         pass
 
 class ListadoMovimientos():
