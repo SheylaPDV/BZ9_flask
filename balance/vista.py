e9b6162fb36ef56bfa13a0a13f2f4ceb2a0a65c4
SheylaPDV <sheylapdv@gmail.com>
2021-11-12 18:23:54 +0100

    comento el codigo para no liarme

--- balance/vista.py
+++ balance/vista.py
@@ -9,17 +9,20 @@ from .modelo import Data_base
 
 @app.route('/')
 def mostrar_tabla():
+    # CREO EL OBJETO DB CON LA RUTA DE LA BASE DE DATOS
     db = Data_base(RUTA)
+    # MOVIMIENTOS ES TODOS LOS MOVMIMENTOS AL CONSULTAR LA BASE DE DATOS
     movimientos = db.consultarSQL('SELECT * FROM MOVEMENTS')
+    #  IMPRMIMO TODOS LOS MOVMIMENOS
     print(movimientos)
-    #  render_template muestra la plantilla en pantalla, en este caso de inicio-html
+    # RENDER TEMPLATE MUESTRA TODOS LOS MOVMIMENTOS EN LA PAGINA DE INICIO
     return render_template("inicio.html", movs=movimientos)
 
     
 @app.route('/purchase', methods=["GET","POST"])
 def mostrar_formulario():
 
-    # COMPROBAR METODO GET O POST
+    #COMPRUEBO SI EL METODO GET O POST
     if request.method == "GET":
         return render_template('purchase.html', datos={
         "from_currency":"",
@@ -29,21 +32,33 @@ def mostrar_formulario():
         )
     
     if request.method == "POST":
+
         datos = {}
         mensaje = ""
+        # OBTENGO EL DIA ACTUAL DEL SISTEMA
         datos["date"] = time.strftime("%y/%m/%d")
+        # OBTENGO LA HORA ACTUAL DEL SISTEMA
         datos["time"] = time.strftime("%H:%M:%S")
+        # UNO LOS DICCIONARIOS
         datos.update(request.form.to_dict())
+        # COMPRUEBO QUE LOS DATOS INTRODUCIDOS NO SON LA MISMA MONEDA
         if datos["from_currency"] == datos["to_currency"]:
             mensaje = "Los monedas no pueden ser iguales"
+        # SI SON DISTINTAS LAS MONEDAS, AÑADO EL MOVIMIMENTO A LA BASE DE DATOS
         else:
+            # GUARDO EN UNA TUPLA TODOS LOS VALORES
             tupla = tuple(datos.values())
             db = Data_base(RUTA)
+            # INSERTO EL MOVIMIENTO EN BASE DE DATOS
             resultado = db.insertarMovimiento(tupla)
+            # COMPRUEBO EL RESULTADO DE INSERTAR EN LA BASE DE DATOS
             if resultado == True:
                 mensaje = "Movimiento creado"
             else:
                 mensaje = "Error al crear el movimiento"
+            # if datos["to_quantity"] == None:
+                # url = f"https://rest.coinapi.io/v1/exchangerate/{"from_currency"}/{to_currency}?apikey={APIKEY}";
+
         
         return render_template("purchase.html", mensaje=mensaje, datos=datos)
 
@@ -60,7 +75,7 @@ def mostrar_formulario():
         
         
         
-        # url = "https://rest.coinapi.io/v1/exchangerate/{from_currency}/{to_currency}?ap"ikey={apikey}".format(
+        # url = "https://rest.coinapi.io/v1/exchangerate/{from_currency}/{to_currency}?apikey={apikey}".format(
         # from_currency = datos['from_currency'],
         # to_currency = datos['to_currency'],
         # apikey = APIKEY)
