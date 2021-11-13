049691dc904f2acd5e7414c5338ca0885d281c86
SheylaPDV <sheylapdv@gmail.com>
2021-11-13 21:47:33 +0100

    reorganizo comentarios

--- balance/vista.py
+++ balance/vista.py
@@ -42,27 +42,32 @@ def mostrar_formulario():
         datos["time"] = time.strftime("%H:%M:%S")
         # UNO LOS DICCIONARIOS
         datos.update(request.form.to_dict())
-        
+        #  compruebo si el campo to_quantity esta vacio
         if datos["to_quantity"] == "":
             
             # COMPRUEBO QUE LOS DATOS INTRODUCIDOS NO SON LA MISMA MONEDA
             if datos["from_currency"] != datos["to_currency"]:
-                
-                url = "https://rest.coinapi.io/v1/exchangerate/{from_currency}/{to_currency}?apikey={apikey}".format(from_currency = datos["from_currency"],to_currency = datos["to_currency"],apikey=APIKEY)
+                # Llamada al api
+                url = "https://rest.coinapi.io/v1/exchangerate/{from_currency}/{to_currency}?apikey={apikey}".format(
+                    from_currency = datos["from_currency"],
+                    to_currency = datos["to_currency"],
+                    apikey=APIKEY)
+                    
                 tipo_cambio = requests.get(url)
-                # guardo el resultado en to_quantitty
+                # guardo el resultado de rate en to_quantitty
                 datos["to_quantity"] = float(tipo_cambio.json()["rate"]) * float(datos["from_quantity"])
             else:
                 mensaje = f"Las monedas no pueden ser iguales"
         else:
+            # convierto en tupla los valores del diccionario
             tupla = tuple(datos.values())
-            print(tupla)
             db = Data_base(RUTA)
-            # INSERTO EL MOVIMIENTO EN BASE DE DATOS
+            # inserto los movimientos en base de datos
             resultado = db.insertarMovimiento(tupla)
+            # compruebo si hubo error al crear el movimiento
             if resultado == True:
                 mensaje = "Movimiento creado"
             else:
                 mensaje = "Error al crear el movimiento"
-                            
+
         return render_template("purchase.html", mensaje=mensaje, datos=datos)
