99a6225b3b3ab7513f9200d219ccd8ce9e169df9
SheylaPDV <sheylapdv@gmail.com>
2021-11-14 17:49:16 +0100

    organizando

--- balance/modelo.py
+++ balance/modelo.py
@@ -1,6 +1,4 @@
 import sqlite3
-from datetime import date
-from datetime import datetime
 
 # CREAMOS CONEXION A BASE DE DATOS
 class Data_base:
@@ -45,53 +43,3 @@ class Data_base:
         conexion.close()
         print("Resultado:", resultado)
         return resultado
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-"""
-class Movimiento():
-    def __init__(self, linea):
-        # LISTA VACIA DE ERRORES EN CASO DE QUE LOS HAYA
-        self.errores = []
-        # DATETIME.NOW TE SACA LA FECHA ACTUAL
-        ahora = datetime.now()
-        try:
-            # EVALUAR SI HAY ERRORES EN LA FECHA
-            self.Fecha = date.fromisoformat(linea["Fecha"])
-            if self.Fecha.strftime("%Y%m%d") > ahora.strftime("%Y%m%d"):
-                self.errores.append("La fecha no puede ser futura")
-                self.Fecha = "---"
-        except ValueError:
-            # SI HAY ERRORES,  AÑADE A LA LISTA VACIA DE ERRORES "FORMATO DE FECHA ERRONEO"
-            self.errores.append("Formato de fecha erroneo")
-            # Y TE PINTA EN LA CASILLA DE FECHA 3 X, PARA SABER QUE NO VALE
-            self.Fecha = "XXX"
-            # LEE CADA LINEA DE MOVMIMENTOS.CSV
-        self.Hora = linea["Hora"]
-        self.From = linea["From"]
-        self.QF = linea["Q"]
-        self.To = linea["To"]
-        self.QT = linea["Q"]
-        pass
-
-class ListadoMovimientos():
-    def __init__(self):
-        # LISTA VACIA DONDE SE INTRODUCEN LOS MOVIMIENTOS
-        self.movimientos=[]
-"""
-   
